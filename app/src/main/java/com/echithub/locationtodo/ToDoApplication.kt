fae0a6841be30c7f7b52c125c41efce78becdcf1
Sirmikky <michael.agbogo@echithub.com>
2022-04-02 06:41:29 +0100

    Initial Commit

---
+++ app/src/main/java/com/echithub/locationtodo/ToDoApplication.kt
@@ -0,0 +1,6 @@
+package com.echithub.locationtodo
+
+import android.app.Application
+
+class ToDoApplication:Application() {
+}
---
+++ app/src/main/java/com/echithub/locationtodo/data/AppDatabase.kt
@@ -0,0 +1,4 @@
+package com.echithub.locationtodo.data
+
+class AppDatabase {
+}
